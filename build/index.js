e8c56f213a0ac2e64850cde1b4d8e8763b8f1d5d
Larissa Lanes <91152234+LarissaLanes@users.noreply.github.com>
2022-04-13 13:46:22 -0300

    Delete build directory

--- build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const PokemonRouter_1 = require("./router/PokemonRouter");
-const app_1 = __importDefault(require("./app"));
-app_1.default.use("/pokemon", PokemonRouter_1.pokemonRouter);
