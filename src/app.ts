e526af32d88c7cffacfd5d829f25ae0d13842402
LarissaLanes <llarissalanep@gmail.com>
2022-04-13 12:49:41 -0300

    deploy

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import cors from "cors";
 import express from "express";
-import { AddressInfo } from "net";
 import dotenv from "dotenv"
 
 dotenv.config();
@@ -10,13 +9,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const server = app.listen(3005, () => {
-    if(server){
-        const address = server.address() as AddressInfo;
-        console.log(`Servidor rodando em http://localhost:${address.port}`)
-    }else{
-        console.error(`falha ao rodar o servidor`)
-    }
+
+app.listen(process.env.PORT || 3005, () => {
+    console.log(`Servidor rodando em http://localhost:3005`)
 })
+
 
 export default app;
